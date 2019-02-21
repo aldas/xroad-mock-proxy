1748d6ddceb6bcd06d1ad27e1f9464cfb4b5897a
toimtoimtoim <desinformatsioon@gmail.com>
2019-02-22 00:23:48 +0200

    simplest reverse proxy ever

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	targetScheme := "https"
+	targetHost := "www.google.com"
+
+	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
+		Scheme: targetScheme,
+		Host:   targetHost,
+	})
+
+	proxy.FlushInterval = 1 * time.Second
+
+	proxy.Director = func(r *http.Request) {
+		// To rewrite Host headers, we need to use ReverseProxy with a custom Director policy.
+		r.Host = targetHost
+		r.URL.Host = r.Host
+		r.URL.Scheme = targetScheme
+	}
+
+	http.Handle("/", proxy)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
 }
